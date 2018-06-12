3e042cc6a5e9f9792546a3155e5cafc41b3b2149
Alexander Mayorov <24964839+ZeroBone@users.noreply.github.com>
2018-06-12 13:35:58 +0300

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+    crypto: {
+        serverKey: "8cfd11687a20d616f0b7dc0ceed00ce12f5f95e2e100c9ff561b0c4117e6e44d"
+    }
+};
---
+++ server/server.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("[PROXY]: Starting server...");
+
+const CrProxy = require("./classes/CrProxy");
+
+const proxy = new CrProxy();
